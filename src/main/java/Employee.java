15467251c67c0e85ebefb5164b1c3ef01ec272a8
Sergey Ogurtsov <sergey.ogurtsov@gmail.com>
2024-09-29 17:18:08 +0300

    add del

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Employee {
     String phoneNumber;
     String phoneName;
@@ -17,5 +19,21 @@ public class Employee {
 
     public String toPrint() {
         return "Номер телефона: " + phoneNumber + " Имя: " + phoneName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Employee employee = (Employee) obj;
+
+        // Предполагаем, что пара (phoneNumber, phoneName) уникальна для каждого объекта
+        return phoneNumber.equals(employee.phoneNumber) && phoneName.equals(employee.phoneName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(phoneNumber, phoneName);
     }
 }
